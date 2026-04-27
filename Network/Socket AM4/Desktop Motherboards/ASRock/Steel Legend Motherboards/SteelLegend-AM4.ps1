5e290a98ed51f96f44d96de838b1273f4aac9f3c
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-26 23:26:14 -0400

    Refactor NICDrivers array syntax and improve output

--- Network/Socket AM4/Desktop Motherboards/ASRock/Steel Legend Motherboards/SteelLegend-AM4.ps1
+++ Network/Socket AM4/Desktop Motherboards/ASRock/Steel Legend Motherboards/SteelLegend-AM4.ps1
@@ -1,13 +1,16 @@
 $NICDrivers = @ (
-{Name = "ASRock Socket AM4 Steel Legend Motherboard Network Card Drivers 1"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/Steel%20Legend%20Motherboards/ASRock-SteelLegend-Intel-LANDrivers.zip"}
-{Name = "ASRock Socket AM4 Steel Legend Motherboard Network Card Drivers 2"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/Steel%20Legend%20Motherboards/ASRock-SteelLegend-Realtek-LANDrivers1.zip"}
-{Name = "ASRock Socket AM4 Steel Legend Motherboard Network Card Drivers 3"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/Steel%20Legend%20Motherboards/ASRock-SteelLegend-Realtek-LANDrivers2.zip"}
+{Name = "ASRock Socket AM4 Steel Legend Motherboard Network Card Drivers 1"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/Steel%20Legend%20Motherboards/ASRock-SteelLegend-Intel-LANDrivers.zip"},
+{Name = "ASRock Socket AM4 Steel Legend Motherboard Network Card Drivers 2"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/Steel%20Legend%20Motherboards/ASRock-SteelLegend-Realtek-LANDrivers1.zip"},
+{Name = "ASRock Socket AM4 Steel Legend Motherboard Network Card Drivers 3"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/Steel%20Legend%20Motherboards/ASRock-SteelLegend-Realtek-LANDrivers2.zip"},
 {Name = "ASRock Socket AM4 Steel Legend Motherboard Network Card Drivers 4"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/Steel%20Legend%20Motherboards/ASRock-SteelLegend-Realtek-LANDrivers3.zip"}
 )
 
+Import-Module -Name OSD -Force
+
 foreach($Driver in $NICDrivers){
 $destination = "C:\download\drivers\Socket AM4\ASRock\Steel Legend\Ethernet"
-Write-Host "`nProcessing: $($Driver.Name) -ForegroundColor Cyan
+Write-Host "`nProcessing: $($Driver.Name)" -ForegroundColor Cyan
+Write-Host
   Save-WebFile -SourceUrL $($Driver.DriverFileURL) -DestinationDirectory $destination
-Write-Host "Completed: $($Driver.Name) download`n" -ForegroundColor Green
+Write-Host "Completed: $($Driver.Name) downloaded`n" -ForegroundColor Green
 }
