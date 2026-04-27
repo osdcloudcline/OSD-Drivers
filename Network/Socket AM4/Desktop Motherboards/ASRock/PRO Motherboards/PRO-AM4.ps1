19454a3629dd2d499d403a1ee995a2e74af9cdca
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-26 23:32:41 -0400

    Rename network card drivers to include 'Ethernet'

--- Network/Socket AM4/Desktop Motherboards/ASRock/PRO Motherboards/PRO-AM4.ps1
+++ Network/Socket AM4/Desktop Motherboards/ASRock/PRO Motherboards/PRO-AM4.ps1
@@ -1,9 +1,9 @@
 $NICDrivers = @ (
-@{Name = "ASRock Socket AM4 PRO Motherboard Network Card Drivers 1"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/PRO%20Motherboards/ASRock-PRO-Intel-LANDrivers.zip"},
-@{Name = "ASRock Socket AM4 PRO Motherboard Network Card Drivers 2"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/PRO%20Motherboards/ASRock-PRO-Realtek-LANDrivers1.zip"},
-@{Name = "ASRock Socket AM4 PRO Motherboard Network Card Drivers 3"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/PRO%20Motherboards/ASRock-PRO-Realtek-LANDrivers2.zip"},
-@{Name = "ASRock Socket AM4 PRO Motherboard Network Card Drivers 4"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/PRO%20Motherboards/ASRock-PRO-Realtek-LANDrivers3.zip"},
-@{Name = "ASRock Socket AM4 PRO Motherboard Network Card Drivers 5"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/PRO%20Motherboards/ASRock-PRO-Realtek-LANDrivers4.zip"}
+@{Name = "ASRock Socket AM4 PRO Motherboard Ethernet Network Card Drivers 1"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/PRO%20Motherboards/ASRock-PRO-Intel-LANDrivers.zip"},
+@{Name = "ASRock Socket AM4 PRO Motherboard Ethernet Network Card Drivers 2"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/PRO%20Motherboards/ASRock-PRO-Realtek-LANDrivers1.zip"},
+@{Name = "ASRock Socket AM4 PRO Motherboard Ethernet Network Card Drivers 3"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/PRO%20Motherboards/ASRock-PRO-Realtek-LANDrivers2.zip"},
+@{Name = "ASRock Socket AM4 PRO Motherboard Ethernet Network Card Drivers 4"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/PRO%20Motherboards/ASRock-PRO-Realtek-LANDrivers3.zip"},
+@{Name = "ASRock Socket AM4 PRO Motherboard Ethernet Network Card Drivers 5"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/PRO%20Motherboards/ASRock-PRO-Realtek-LANDrivers4.zip"}
 )
 
 Import-Module -Name OSD -Force
