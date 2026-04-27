f58bc5288bc21e0dc2eff90478c813198423eb33
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-26 23:08:38 -0400

    Add PowerShell script for ASRock Steel Legend drivers

---
+++ Network/Socket AM4/Desktop Motherboards/ASRock/Steel Legend Motherboards/SteelLegend-AM4.ps1
@@ -0,0 +1,13 @@
+$NICDrivers = @ (
+{Name = "ASRock Socket AM4 Steel Legend Motherboard Network Card Drivers 1"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/Steel%20Legend%20Motherboards/ASRock-SteelLegend-Intel-LANDrivers.zip"}
+{Name = "ASRock Socket AM4 Steel Legend Motherboard Network Card Drivers 2"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/Steel%20Legend%20Motherboards/ASRock-SteelLegend-Realtek-LANDrivers1.zip"}
+{Name = "ASRock Socket AM4 Steel Legend Motherboard Network Card Drivers 3"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/Steel%20Legend%20Motherboards/ASRock-SteelLegend-Realtek-LANDrivers2.zip"}
+{Name = "ASRock Socket AM4 Steel Legend Motherboard Network Card Drivers 4"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/Steel%20Legend%20Motherboards/ASRock-SteelLegend-Realtek-LANDrivers3.zip"}
+)
+
+foreach($Driver in $NICDrivers){
+$destination = "C:\download\drivers\Socket AM4\ASRock\Steel Legend\Ethernet"
+Write-Host "`nProcessing: $($Driver.Name) -ForegroundColor Cyan
+  Save-WebFile -SourceUrL $($Driver.DriverFileURL) -DestinationDirectory $destination
+Write-Host "Completed: $($Driver.Name) download`n" -ForegroundColor Green
+}
