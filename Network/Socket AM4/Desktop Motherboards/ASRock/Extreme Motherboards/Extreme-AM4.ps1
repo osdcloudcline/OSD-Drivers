2f003b4fba66da2a602ea2fd01f8f0d1c11e43ba
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-27 00:52:48 -0400

    Enhance driver download and extraction process
    Added functionality to download and extract drivers for ASRock Socket AM4 Extreme Motherboard.

--- Network/Socket AM4/Desktop Motherboards/ASRock/Extreme Motherboards/Extreme-AM4.ps1
+++ Network/Socket AM4/Desktop Motherboards/ASRock/Extreme Motherboards/Extreme-AM4.ps1
@@ -3,7 +3,12 @@ $NICDrivers = @(
     @{Name = "ASRock Socket AM4 Extreme Motherboard Intel Ethernet Network Card Drivers"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/Extreme%20Motherboards/ASRock-Extreme-Intel-NetworkDrivers.zip"}
 )
 
+$DriverFileNames = @(
+@{FileName = "ASRock-Extreme-Intel-NetworkDrivers.zip")
+)
+
 Import-Module -Name OSD -Force
+Import-Module -Name 7Zip4Powershell -Force
 
 foreach($Driver in $NICDrivers){
     $destination = "C:\download\drivers\Socket AM4\ASRock\Extreme\Ethernet"
@@ -16,3 +21,13 @@ foreach($Driver in $NICDrivers){
     
     Write-Host "Completed: $($Driver.Name) downloaded`n" -ForegroundColor Green
 }
+
+
+foreach($FileName in $DriverFileNames){
+    $expanddir = "C:\download\drivers\Socket AM4\ASRock\Extreme\Ethernet\extract"
+    Write-Host "`nProcessing: $($Driver.Name) extraction" -ForegroundColor Cyan
+    Write-Host
+    Expand-7Zip -ArchiveFileName "$destination\$($FileName.FileName)"  -TargetPath $expanddir
+
+    Write-Host "Completed: $($FileName.FileName) extracted`n" -ForegroundColor Green
+    }
