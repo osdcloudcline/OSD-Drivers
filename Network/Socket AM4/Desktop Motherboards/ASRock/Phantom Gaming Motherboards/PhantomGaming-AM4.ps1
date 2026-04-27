fc5ffb7b2d11a356515aa480773de9dde10ef4ee
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-26 23:24:53 -0400

    Refactor NICDrivers array syntax in PowerShell script

--- Network/Socket AM4/Desktop Motherboards/ASRock/Phantom Gaming Motherboards/PhantomGaming-AM4.ps1
+++ Network/Socket AM4/Desktop Motherboards/ASRock/Phantom Gaming Motherboards/PhantomGaming-AM4.ps1
@@ -1,14 +1,17 @@
 $NICDrivers = @ (
-{Name = "ASRock Socket AM4 Phantom Gaming Motherboard Network Card Drivers 1"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/Phantom%20Gaming%20Motherboards/ASRock-PhantomGaming-Intel-LANDrivers1.zip"}
-{Name = "ASRock Socket AM4 Phantom Gaming Motherboard Network Card Drivers 2"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/Phantom%20Gaming%20Motherboards/ASRock-PhantomGaming-Intel-LANDrivers2.zip"}
-{Name = "ASRock Socket AM4 Phantom Gaming Motherboard Network Card Drivers 3"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/Phantom%20Gaming%20Motherboards/ASRock-PhantomGaming-Realtek-LANDriver1.zip"}
-{Name = "ASRock Socket AM4 Phantom Gaming Motherboard Network Card Drivers 4"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/Phantom%20Gaming%20Motherboards/ASRock-PhantomGaming-Realtek-LANDriver2.zip"}
-{Name = "ASRock Socket AM4 Phantom Gaming Motherboard Network Card Drivers 5"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/Phantom%20Gaming%20Motherboards/ASRock-PhantomGaming-Realtek-LANDrivers3.zip"}
+@{Name = "ASRock Socket AM4 Phantom Gaming Motherboard Network Card Drivers 1"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/Phantom%20Gaming%20Motherboards/ASRock-PhantomGaming-Intel-LANDrivers1.zip"},
+@{Name = "ASRock Socket AM4 Phantom Gaming Motherboard Network Card Drivers 2"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/Phantom%20Gaming%20Motherboards/ASRock-PhantomGaming-Intel-LANDrivers2.zip"},
+@{Name = "ASRock Socket AM4 Phantom Gaming Motherboard Network Card Drivers 3"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/Phantom%20Gaming%20Motherboards/ASRock-PhantomGaming-Realtek-LANDriver1.zip"},
+@{Name = "ASRock Socket AM4 Phantom Gaming Motherboard Network Card Drivers 4"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/Phantom%20Gaming%20Motherboards/ASRock-PhantomGaming-Realtek-LANDriver2.zip"},
+@{Name = "ASRock Socket AM4 Phantom Gaming Motherboard Network Card Drivers 5"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/Phantom%20Gaming%20Motherboards/ASRock-PhantomGaming-Realtek-LANDrivers3.zip"}
 )
+
+Import-Module -Name OSD -Force
 
 foreach($Driver in $NICDrivers){
 $destination = "C:\download\drivers\Socket AM4\ASRock\Phantom Gaming\Ethernet"
-Write-Host "`nProcessing: $($Driver.Name) -ForegroundColor Cyan
+Write-Host "`nProcessing: $($Driver.Name)" -ForegroundColor Cyan
+Write-Host
   Save-WebFile -SourceUrL $($Driver.DriverFileURL) -DestinationDirectory $destination
-Write-Host "Completed: $($Driver.Name) download`n" -ForegroundColor Green
+Write-Host "Completed: $($Driver.Name) downloaded`n" -ForegroundColor Green
 }
