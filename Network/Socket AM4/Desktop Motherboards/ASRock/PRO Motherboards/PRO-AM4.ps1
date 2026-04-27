2b6ab76f8b5bacfd4d451363b49ca27233cfa115
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-26 23:00:27 -0400

    Create PRO-AM4.ps1 for downloading network drivers
    Added a PowerShell script to download ASRock PRO motherboard network drivers.

---
+++ Network/Socket AM4/Desktop Motherboards/ASRock/PRO Motherboards/PRO-AM4.ps1
@@ -0,0 +1,14 @@
+$NICDrivers = @ (
+{Name = "ASRock Socket AM4 PRO Motherboard Network Card Drivers 1"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/PRO%20Motherboards/ASRock-PRO-Intel-LANDrivers.zip"}
+{Name = "ASRock Socket AM4 PRO Motherboard Network Card Drivers 2"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/PRO%20Motherboards/ASRock-PRO-Realtek-LANDrivers1.zip"}
+{Name = "ASRock Socket AM4 PRO Motherboard Network Card Drivers 3"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/PRO%20Motherboards/ASRock-PRO-Realtek-LANDrivers2.zip"}
+{Name = "ASRock Socket AM4 PRO Motherboard Network Card Drivers 4"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/PRO%20Motherboards/ASRock-PRO-Realtek-LANDrivers3.zip"}
+{Name = "ASRock Socket AM4 PRO Motherboard Network Card Drivers 5"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/PRO%20Motherboards/ASRock-PRO-Realtek-LANDrivers4.zip"}
+)
+
+foreach($Driver in $NICDrivers){
+$destination = "C:\download\drivers\Socket AM4\ASRock\PRO\Ethernet"
+Write-Host "`nProcessing: $($Driver.Name) -ForegroundColor Cyan
+  Save-WebFile -SourceUrL $($Driver.DriverFileURL) -DestinationDirectory $destination
+Write-Host "Completed: $($Driver.Name) download`n" -ForegroundColor Green
+}
