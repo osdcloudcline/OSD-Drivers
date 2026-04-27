4b615dc471460cc8ab8c69f4ce748a1c5084ee2f
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-27 07:10:13 -0400

    Import 7Zip4Powershell module for driver extraction
    Added import for 7Zip4Powershell module to handle driver extraction.

--- Network/Socket AM4/Desktop Motherboards/ASRock/Taichi Motherboards/Taichi-AM4.ps1
+++ Network/Socket AM4/Desktop Motherboards/ASRock/Taichi Motherboards/Taichi-AM4.ps1
@@ -7,6 +7,7 @@ $DriverFileNames = @(
 )
 
 Import-Module -Name OSD -Force
+Import-Module -Name 7Zip4Powershell -Force
 
 foreach($Driver in $NICDrivers){
 $destination = "C:\download\drivers\Socket AM4\ASRock\Taichi\Ethernet"
