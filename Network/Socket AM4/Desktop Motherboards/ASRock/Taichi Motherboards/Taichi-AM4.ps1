eff391802fdebd99d55cc0c8ebe3467a8bd60991
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-26 23:29:41 -0400

    Add PowerShell script for ASRock Taichi drivers

---
+++ Network/Socket AM4/Desktop Motherboards/ASRock/Taichi Motherboards/Taichi-AM4.ps1
@@ -0,0 +1,13 @@
+$NICDrivers = @ (
+@{Name = "ASRock Socket AM4 Taichi Motherboard Network Card Drivers"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/Taichi%20Motherboards/ASRock-Taichi-Intel-LANDrivers.zip"},
+)
+
+Import-Module -Name OSD -Force
+
+foreach($Driver in $NICDrivers){
+$destination = "C:\download\drivers\Socket AM4\ASRock\Taichi\Ethernet"
+Write-Host "`nProcessing: $($Driver.Name)" -ForegroundColor Cyan
+Write-Host
+  Save-WebFile -SourceUrL $($Driver.DriverFileURL) -DestinationDirectory $destination
+Write-Host "Completed: $($Driver.Name) downloaded`n" -ForegroundColor Green
+}
