920ba6e0aa8f5ed5fdfeb6308dadf6ca4d6cd516
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-26 22:53:29 -0400

    Add PowerShell script for ASRock Extreme drivers

---
+++ Network/Socket AM4/Desktop Motherboards/ASRock/Extreme Motherboards/Extreme-AM4.ps1
@@ -0,0 +1,11 @@
+$NICDrivers = @ (
+{Name = "ASRock Extreme Motherboard Intel Network Card Drivers"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/Extreme%20Motherboards/ASRock-Extreme-Intel-NetworkDrivers.zip"}
+
+)
+
+foreach($DriverFileName in $NICDrivers){
+$destination = "C:\download\drivers\Socket AM4\ASRock\Extreme\Ethernet"
+Write-Host "`nProcessing: $($Driver.Name) -ForegroundColor Cyan
+  Save-WebFile -SourceUrL $($Driver.DriverFileURL) -DestinationDirectory $destination
+Write-Host "Completed: $($Driver.Name) download`n" -ForegroundColor Green
+}
