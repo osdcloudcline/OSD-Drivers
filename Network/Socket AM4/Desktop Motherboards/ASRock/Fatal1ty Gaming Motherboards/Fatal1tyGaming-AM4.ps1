debe294590e91bbf74c96ad7367c6e3dd9091eb4
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-26 22:55:36 -0400

    Add PowerShell script for ASRock Fatal1ty drivers

---
+++ Network/Socket AM4/Desktop Motherboards/ASRock/Fatal1ty Gaming Motherboards/Fatal1tyGaming-AM4.ps1
@@ -0,0 +1,11 @@
+$NICDrivers = @ (
+{Name = "ASRock Fatal1ty Gaming Motherboard Network Card Drivers 1"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/Fatal1ty%20Gaming%20Motherboards/ASRock-Fatal1tyGaming-Realtek-LANDrivers1.zip"}
+{Name = "ASRock Fatal1ty Gaming Motherboard Network Card Drivers 2"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM4/Desktop%20Motherboards/ASRock/Fatal1ty%20Gaming%20Motherboards/ASRock-Fatal1tyGaming-Realtek-LANDrivers2.zip"}
+)
+
+foreach($Driver in $NICDrivers){
+$destination = "C:\download\drivers\Socket AM4\ASRock\Fatal1ty Gaming\Ethernet"
+Write-Host "`nProcessing: $($Driver.Name) -ForegroundColor Cyan
+  Save-WebFile -SourceUrL $($Driver.DriverFileURL) -DestinationDirectory $destination
+Write-Host "Completed: $($Driver.Name) download`n" -ForegroundColor Green
+}
