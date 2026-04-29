62232b8190121de07e5a481db1ee021ebd01f3fe
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-29 07:37:44 -0400

    Add PowerShell script for Gigabyte AM5 drivers

---
+++ Network/Socket AM5/Gigabyte/UD - Ultra Durable Motherboards/UltraDurable-AM5.ps1
@@ -0,0 +1,35 @@
+$NICDrivers = @(
+    @{Name = "Gigabyte Socket AM5 Ultra Durable Motherboard Intel Ethernet Network Card Drivers 1"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM5/Gigabyte/UD%20-%20Ultra%20Durable%20Motherboards/Gigabyte-AM5-UltraDurable-A620-EthernetDrivers1.zip"},
+    @{Name = "Gigabyte Socket AM5 Ultra Durable Motherboard Intel Ethernet Network Card Drivers 2"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM5/Gigabyte/UD%20-%20Ultra%20Durable%20Motherboards/Gigabyte-AM5-UltraDurable-A620-EthernetDrivers2.zip"},
+    @{Name = "Gigabyte Socket AM5 Ultra Durable Motherboard Intel Ethernet Network Card Drivers 3"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM5/Gigabyte/UD%20-%20Ultra%20Durable%20Motherboards/Gigabyte-AM5-UltraDurable-B650-EthernetDrivers1.zip"},
+    @{Name = "Gigabyte Socket AM5 Ultra Durable Motherboard Intel Ethernet Network Card Drivers 4"; DriverFileURL = "https://github.com/osdcloudcline/OSD-Drivers/raw/refs/heads/main/Network/Socket%20AM5/Gigabyte/UD%20-%20Ultra%20Durable%20Motherboards/Gigabyte-AM5-UltraDurable-B650-EthernetDrivers2.zip"}
+   
+)
+
+$DriverFileNames = @(
+    @{FileName = "Gigabyte-AM5-UltraDurable-A620-EthernetDrivers1.zip"},
+    @{FileName = "Gigabyte-AM5-UltraDurable-A620-EthernetDrivers2.zip"}, 
+    @{FileName = "Gigabyte-AM5-UltraDurable-B650-EthernetDrivers1.zip"},
+    @{FileName = "Gigabyte-AM5-UltraDurable-B650-EthernetDrivers2.zip"}
+)
+
+Import-Module -Name OSD -Force
+Import-Module -Name 7Zip4Powershell -Force
+
+$destination = "C:\download\drivers\Socket AM5\Gigabyte\Ultra Durable\Ethernet"
+
+foreach($Driver in $NICDrivers){
+    Write-Host "`nProcessing: $($Driver.Name)" -ForegroundColor Cyan
+    Write-Host
+    Save-WebFile -SourceUrL $($Driver.DriverFileURL) -DestinationDirectory $destination
+    Write-Host "Completed: $($Driver.Name) downloaded`n" -ForegroundColor Green
+}
+
+foreach($FileItem in $DriverFileNames){
+    $expanddir = "C:\download\drivers\Socket AM5\Gigabyte\Ultra Durable\Ethernet\extract"
+    # Fixed: Changed $Driver.Name to $FileItem.FileName since $Driver is out of scope
+    Write-Host "Processing: $($FileItem.FileName) extraction" -ForegroundColor Cyan
+    Write-Host
+    Expand-7Zip -ArchiveFileName "$destination\$($FileItem.FileName)" -TargetPath $expanddir
+    Write-Host "Completed: $($FileItem.FileName) extracted`n" -ForegroundColor Green
+}
