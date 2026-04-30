d42864810cabf770f00c15744413a1fa779b7ac2
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-30 13:22:00 -0400

    Add additional AMD chipsets to the list

--- Chipset/AMD/AMDChipsets.ps1
+++ Chipset/AMD/AMDChipsets.ps1
@@ -1,6 +1,8 @@
 # Define the AMD Chipsets Software using Hash Tables @{ }
 $AMDChipsets = @(
-   @{ Name = "AMD Ryzen Chipset Drivers"; ID = "amd-ryzen-chipset" }
+   @{ Name = "AMD Ryzen Chipset Drivers"; ID = "amd-ryzen-chipset" },
+   @{ Name = "AMD Ryzen Master Utility"; ID = "amd-ryzen-master" },
+   @{ Name = "AMD Software Adrenalin Edition 26.3.1"; ID = "amd-software-adrenalin-edition" }
    
 )
 
