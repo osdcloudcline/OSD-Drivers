20e3ae7b5ed5b9417e0e870a3f0e9da2905db9d7
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-05-01 07:25:51 -0400

    Add processing message and network driver invocation

--- ALL - Main Scripts/Socket AM4/ASRock/ALL-Extreme-AM4.ps1
+++ ALL - Main Scripts/Socket AM4/ASRock/ALL-Extreme-AM4.ps1
@@ -1,1 +1,5 @@
+Write-Host "Processing: ASRock AMD Socket AM4 Extreme Motherboard Network Drivers" -ForegroundColor Cyan
+
+$ExtremeAM4NICDrivers = Invoke-WebRequest("")
+Invoke-Expression $($ExtremeAM4NICDrivers.Content)
 
